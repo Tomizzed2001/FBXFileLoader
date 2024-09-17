107d062e91cdc0ee571d59962873c12b3d16b0c9
Tom Jackson <tom.jackson1711@gmail.com>
2024-09-17 15:53:00 +0100

    Import the fbx file
    Added the necessary code to initialise the FBX SDK and read in an FBX file.

---
+++ premake5.lua
@@ -0,0 +1,24 @@
+workspace "FBXFileLoader"
+    language "C++"
+    cppdialect "C++20"
+    platforms { "x64" }
+    configurations { "debug", "release" }
+
+    --configurations
+    filter "debug"
+        defines { "_DEBUG=1" }
+        symbols "On"
+
+    filter "release"
+        defines { "NDEBUG=1" }
+        optimize "On"
+
+    filter "*"
+
+    -- Include files (The default directory)
+    includedirs{"C:/Program Files/Autodesk/FBX/FBX SDK/2020.3.7/include"}
+
+project "FileLoader"
+    kind "ConsoleApp"
+    location "src"
+    files {"src/**.cpp", "src/**.hpp"}
---
+++ src/FBXFileLoader.cpp
@@ -0,0 +1,56 @@
+#include <fbxsdk.h>
+#include <filesystem>
+#include <iostream>
+
+// Link the libraries necessary for the execution mode
+// The file paths are based on the default location for the 2020.3.7 version of the SDK
+#if _DEBUG
+#pragma comment (lib, "C:\\Program Files\\Autodesk\\FBX\\FBX SDK\\2020.3.7\\lib\\x64\\debug\\libfbxsdk-md.lib")
+#pragma comment (lib, "C:\\Program Files\\Autodesk\\FBX\\FBX SDK\\2020.3.7\\lib\\x64\\debug\\libxml2-md.lib")
+#pragma comment (lib, "C:\\Program Files\\Autodesk\\FBX\\FBX SDK\\2020.3.7\\lib\\x64\\debug\\zlib-md.lib")
+#else
+#pragma comment (lib, "C:\\Program Files\\Autodesk\\FBX\\FBX SDK\\2020.3.7\\lib\\x64\\release\\libfbxsdk-md.lib")
+#pragma comment (lib, "C:\\Program Files\\Autodesk\\FBX\\FBX SDK\\2020.3.7\\lib\\x64\\release\\libxml2-md.lib")
+#pragma comment (lib, "C:\\Program Files\\Autodesk\\FBX\\FBX SDK\\2020.3.7\\lib\\x64\\release\\zlib-md.lib")
+#endif
+
+int main() {
+
+    // The name of the fbx file to open
+    const char* lFilename = "SunTemple/SunTemple.fbx";
+
+    // Create the FBX Memory Manager
+    FbxManager* memoryManager = FbxManager::Create();
+
+    // Create the IO settings for the import settings
+    FbxIOSettings* ioSettings = FbxIOSettings::Create(memoryManager, IOSROOT);
+    
+    // Assign the IO settings to the memory manager
+    memoryManager->SetIOSettings(ioSettings);
+
+    // Adjust the import settings in the memory manager so that only the required data is imported
+    (*(memoryManager->GetIOSettings())).SetBoolProp(IMP_FBX_LINK, false);
+    (*(memoryManager->GetIOSettings())).SetBoolProp(IMP_FBX_SHAPE, false);
+    (*(memoryManager->GetIOSettings())).SetBoolProp(IMP_FBX_GOBO, false);
+    (*(memoryManager->GetIOSettings())).SetBoolProp(IMP_FBX_ANIMATION, false);
+
+    // Create and initialise the importer using the above settings
+    FbxImporter* importer = FbxImporter::Create(memoryManager, "");
+
+    // Use the first argument as the filename for the importer.
+    if (!importer->Initialize(lFilename, -1, memoryManager->GetIOSettings())) {
+        throw std::runtime_error("Failed to initialise the importer.");
+    }
+
+    // Create a scene object to store the data from the .fbx file
+    FbxScene* scene = FbxScene::Create(memoryManager, "Scene");
+
+    // Import the .fbx file and store within the scene object created and destroy the importer.
+    importer->Import(scene);
+    importer->Destroy();
+
+    // Destroy the SDK manager and all the other objects it was handling.
+    memoryManager->Destroy();
+
+    return 0;
+}
